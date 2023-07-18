b12f8025016e74a5da86d68fbe552949f580c4a0
murkrow02 <marcocoppola02.mc@gmail.com>
2023-07-18 10:01:35 +0000

    Scheduler to backup dbs

--- backup-db.sh
+++ backup-db.sh
@@ -10,24 +10,29 @@ backup_path=$5
 # Cd to path
 cd $backup_path
 
-# Create folder with month name if does not exist
-current_month=$(date +%B)
-mkdir -p $current_month
-
 # Ensure permission to backup folder
-chown -R mssql $backup_path
+chown  mssql $backup_path
 
 # Get current date time
 current_datetime=$(date +"%Y-%m-%d(%H:%M:%S)")
 
 # Connect to SQL Server and perform backup
-sqlcmd -S $server -U $username -P $password -Q "BACKUP DATABASE $database TO DISK='$backup_path/$current_month/backup-$current_datetime.bak'"
+sqlcmd -S $server -U $username -P $password -Q "BACKUP DATABASE $database TO DISK='$backup_path/backup-$current_datetime.bak'"
 
-# Remove all dirs EXCEPT from current month
-directories=$(ls -d */)
-for dir in $directories; do
-  dir_name=$(basename "$dir")
-  if [[ $dir_name != $current_month ]]; then
-	rm -r $dir_name
-  fi
-done
+# Maximum number of files to keep
+max_files=5
+
+# Change to the backup directory
+cd "$backup_path" || exit
+
+# List files in the directory by modification time, oldest first
+files=($(ls -t))
+
+# Delete excess files if the count exceeds the maximum
+if [ ${#files[@]} -gt $max_files ]; then
+  files_to_delete=$(( ${#files[@]} - max_files ))
+  for ((i=0; i<$files_to_delete; i++)); do
+    rm "${files[${#files[@]}-i-1]}"
+    echo "Deleted file: ${files[${#files[@]}-i-1]}"
+  done
+fi
